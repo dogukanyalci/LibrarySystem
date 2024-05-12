544416ac44e51b938398f33fb9d9fed9f4f44ee7
dogukanyalci <yalcidogukan965@gmail.com>
2024-05-12 14:15:21 +0300

    Update-Core

---
+++ Library-Core/Entities/Concrete/Publisher.cs
@@ -0,0 +1,19 @@
+﻿using Library_Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Core.Entities.Concrete
+{
+    public class Publisher : BaseEntity
+    {
+        public Publisher()
+        {
+            Authors = new List<AuthorPublisher>();
+        }
+        public string Name { get; set; }
+        public List<AuthorPublisher> Authors { get; set; }
+    }
+}
