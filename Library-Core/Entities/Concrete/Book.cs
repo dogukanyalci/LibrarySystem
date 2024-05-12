8aacc3a20e069fbf5bda1060ed9442a63cde78e9
Hazar Seven <hazar.seven@gazi.edu.tr>
2024-05-12 16:51:54 +0300

    SeedDatav3

--- Library-Core/Entities/Concrete/Book.cs
+++ Library-Core/Entities/Concrete/Book.cs
@@ -14,13 +14,13 @@ namespace Library_Core.Entities.Concrete
         public int AuthorId { get; set; }
         public int GenreId { get; set; }
         public int PublisherId { get; set; }
-        public DateTime PublishYear { get; set; }
+        public int PublishYear { get; set; }
         public int PageCount { get; set; }
         public int StockCount { get; set; }
         public int BorrowedCount { get; set; }
         public int Rating { get; set; }
         public string Language { get; set; }
-        public string? ImagePath { get; set; }
+        public string? ImageUrl { get; set; }
         public bool? IsFavorited { get; set; }
         public Genre Genre { get; set; }
         public Author Author { get; set; }
